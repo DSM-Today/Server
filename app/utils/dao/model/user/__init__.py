f8cc93b980823bf62570c89a99f97c5e0aa3dfff
osangu <osangu012@gmail.com>
2022-11-10 09:24:33 +0900

    ADD: user Model

---
+++ app/utils/dao/model/user/__init__.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, BINARY, CHAR, VARCHAR, TEXT, DATE, BOOLEAN
+
+from app.utils.dao.model import Base
+
+
+class User(Base):
+    __tablename__ = 'tbl_user'
+
+    id = Column(BINARY(16), nullable=False, primary_key=True)
+    email = Column(VARCHAR(30), nullable=False, unique=True)
+    name = Column(CHAR(5), nullable=False)
+    image_path = Column(VARCHAR(255), nullable=False)
+    introduce = Column(TEXT, nullable=True)
+    birth_day = Column(DATE, nullable=False)
+    can_person = Column(BOOLEAN, nullable=False, default=False)
