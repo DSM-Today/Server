9ad89fe361631e5cc4645786dbd348c7437e76d7
osangu <osangu012@gmail.com>
2022-11-13 16:07:39 +0900

    UPDATE: date to int

--- app/utils/type_changer.py
+++ app/utils/type_changer.py
@@ -1,5 +1,9 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 def str_to_date(origin_str: str):
     return datetime.strptime(origin_str, '%Y%m%d').date()
+
+
+def date_to_int(from_data: date):
+    return int(from_data.strftime('%Y%m%d').replace('-', ''))
