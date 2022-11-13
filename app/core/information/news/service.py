3235c2537bde480ed3785f11dbd383bf0f33a604
osangu <osangu012@gmail.com>
2022-11-13 16:06:56 +0900

    ADD: news api

---
+++ app/core/information/news/service.py
@@ -0,0 +1,37 @@
+from uuid import uuid4, UUID
+
+from app.utils.security.token import get_user_id
+
+from app.utils.dao.cqrs.subject.comand import create_subject
+from app.utils.dao.cqrs.bookmark.command import create_bookmark, delete_bookmark_by_user_id
+
+from app.utils.dataset.crawler.information.news import news_crawler
+
+
+def query_news():
+    _id = uuid4().bytes
+    news = news_crawler.crawl()
+
+    create_subject(_id, 'NEWS', '오늘의 뉴스', 'INFORMATION')
+
+    return news
+
+
+def insert_news_bookmark(token: str):
+    user_id = UUID(get_user_id(token)).hex
+
+    create_bookmark(
+        user_id=user_id,
+        name='NEWS',
+        kind='INFORMATION',
+        title='오늘의 뉴스',
+    )
+
+
+def delete_my_news_bookmark(token: str):
+    user_id = UUID(get_user_id(token)).hex
+
+    delete_bookmark_by_user_id(user_id, 'NEWS')
+
+
+
