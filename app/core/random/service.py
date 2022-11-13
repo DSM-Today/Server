968783574e77001e16735b52fa4818c9dbe627c8
osangu <osangu012@gmail.com>
2022-11-13 16:45:57 +0900

    UPDATE: key

--- app/core/random/service.py
+++ app/core/random/service.py
@@ -2,4 +2,7 @@ from app.utils.dao.cqrs.subject.query import query_subject_title_list_by_kind
 
 
 def query_random_subject_list():
-    return query_subject_title_list_by_kind('RANDOM')
+    return {
+        'random_subject_list': query_subject_title_list_by_kind('RANDOM')
+    }
+
