1f18ea58db9d490ac53cafe7ab1c12ebd4fa5a26
osangu <osangu012@gmail.com>
2022-11-13 16:17:33 +0900

    UPDATE: include router

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 from app.core.auth import auth_router
 
+from app.core.information import information_router
+from app.core.information.news import news_router
+from app.core.information.lotto import lotto_router
+
+from app.core.random import random_router
+from app.core.random.luck import luck_router
+
 
 def create_app():
     app = FastAPI(
@@ -10,5 +17,14 @@ def create_app():
     )
 
     app.include_router(auth_router)
+
+    # random
+    app.include_router(random_router)
+    app.include_router(luck_router)
+
+    # information
+    app.include_router(news_router)
+    app.include_router(lotto_router)
+    app.include_router(information_router)
 
     return app
