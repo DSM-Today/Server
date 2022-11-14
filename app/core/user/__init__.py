30f2ffb703128278fcf7a8eac3758c7971364fe6
osangu <osangu012@gmail.com>
2022-11-14 15:26:34 +0900

    ADD: query user profile

--- app/core/user/__init__.py
+++ app/core/user/__init__.py
@@ -2,7 +2,7 @@ from fastapi import Depends, APIRouter
 
 from app.utils.security import oauth2_scheme
 
-from app.core.user.service import query_my_bookmark_list
+from app.core.user.service import query_my_bookmark_list, query_my_profile
 
 user_router = APIRouter(
     prefix='/user'
@@ -12,3 +12,8 @@ user_router = APIRouter(
 @user_router.get('/list')
 def get_my_bookmark_list(token: str = Depends(oauth2_scheme)):
     return query_my_bookmark_list(token)
+
+
+@user_router.get('/profile')
+def show_my_profile(token: str = Depends(oauth2_scheme)):
+    return query_my_profile(token)
--- app/core/user/service.py
+++ app/core/user/service.py
@@ -1,6 +1,7 @@
 from app.utils.security.token import get_user_id
 
 from app.utils.dao.cqrs.bookmark.query import query_my_bookmark_list_by_id
+from app.utils.dao.cqrs.user.query import query_user_by_id
 
 
 def query_my_bookmark_list(token: str):
@@ -8,4 +9,17 @@ def query_my_bookmark_list(token: str):
 
     return {
         'bookmark_list': query_my_bookmark_list_by_id(user_id)
+    }
+
+
+def query_my_profile(token: str):
+    user = query_user_by_id(
+        get_user_id(token)
+    )
+
+    return {
+        'name': user.name,
+        'birth_day': user.birth_day,
+        'image_path': user.image_path,
+        'introduction': user.introduce
     }
