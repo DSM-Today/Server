3f36d951e5e15fe2eef66ccae1416cccede68c9e
osangu <osangu012@gmail.com>
2022-11-14 13:12:16 +0900

    ADD: query bookmark list by user_id

---
+++ app/utils/dao/cqrs/bookmark/query.py
@@ -0,0 +1,16 @@
+from uuid import UUID
+
+from app.utils.dao.cqrs import dao
+from sqlalchemy.sql import text
+
+
+def query_my_bookmark_list_by_id(user_id: UUID.hex):
+    sql = f"select subject_name, subject_title, (" \
+          f"select count(subject_name) as count from tbl_bookmark tb2 where tb2.subject_name like tb1.subject_name) as amount " \
+          f"from tbl_bookmark tb1" \
+          f" where user_id like UNHEX('{user_id}')"
+
+    with dao.execute_query() as engine:
+        return engine.execute(text(
+            sql
+        )).all()
