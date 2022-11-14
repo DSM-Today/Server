99c166b74dbe9258de6151be28c390c1ffe9a76b
osangu <osangu012@gmail.com>
2022-11-14 09:15:16 +0900

    UPDATE: user command

--- app/utils/dao/cqrs/user/command.py
+++ app/utils/dao/cqrs/user/command.py
@@ -5,24 +5,14 @@ from app.utils.dao.cqrs import dao
 from app.utils.dao.model.user import User
 
 
-def save_user(email: str, name: str, image_path: str, birth_day: date, can_person: bool,
-              introduce: str, _id: UUID.bytes):
-    if _id is None:
-        _id = uuid4().bytes
-
-    if introduce is None:
-        introduce = name + ' 지구를 부술 유일한 인간..크큭'
-
+def save_user(email: str, name: str, image_path: str, can_person: bool):
     with dao.session_scope() as session:
-        session.add(
-            User(
-                id=_id,
-                email=email,
-                name=name,
-                image_path=image_path,
-                introduce=introduce,
-                birth_day=birth_day,
-                can_person=can_person
-            )
+        user = User(
+            id=uuid4().bytes,
+            email=email,
+            name=name,
+            image_path=image_path,
+            can_person=can_person
         )
-        session.commit()
+        session.add(user)
+        return user
