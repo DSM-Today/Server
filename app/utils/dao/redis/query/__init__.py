324f31531ac1ef1261140ec0832dce02a6fc34ad
osangu <osangu012@gmail.com>
2022-11-15 23:58:19 +0900

    UPDATE: handling attribute error

--- app/utils/dao/redis/query/__init__.py
+++ app/utils/dao/redis/query/__init__.py
@@ -2,4 +2,9 @@ from app.utils.dao.redis import Redis
 
 
 def get_value_by_user_id(uid):
-    return Redis.get(uid).decode("utf-8")
+    token = Redis.get(uid)
+
+    if token is not None:
+        token = token.decode('utf-8')
+
+    return token
