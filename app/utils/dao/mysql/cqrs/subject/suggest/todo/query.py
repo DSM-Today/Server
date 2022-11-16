b764a1e663f6de6ca76cd2bd54e1b9ca507d2b8f
osangu <osangu012@gmail.com>
2022-11-16 21:15:57 +0900

    ADD: query my todo list

--- app/utils/dao/mysql/cqrs/subject/suggest/todo/query.py
+++ app/utils/dao/mysql/cqrs/subject/suggest/todo/query.py
@@ -12,3 +12,13 @@ def query_todo_list(user_id: str):
                 " from tbl_todo td left join tbl_my_todo tmt on td.id = tmt.todo_id;"
 
         return execute(text(query)).all()
+
+
+def query_my_todo_list(user_id: str):
+    with dao.execute_query() as execute:
+        query = "select HEX(tt.id) as todo_id, tt.content " \
+                "from tbl_my_todo mtd " \
+                "join tbl_todo tt on tt.id = mtd.todo_id " \
+                f"where user_id like UNHEX('{user_id}')"
+
+        return execute(text(query)).all()
