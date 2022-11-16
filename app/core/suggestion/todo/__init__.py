65f2592c3a6461827819f0a38c9e29b5aa356796
osangu <osangu012@gmail.com>
2022-11-16 21:16:19 +0900

    ADD: 내 할 일 리스트 API

--- app/core/suggestion/todo/__init__.py
+++ app/core/suggestion/todo/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from app.utils.security import oauth2_scheme
 
-from app.core.suggestion.todo.service import query_all_todo_list, insert_to_my_todo, delete_from_my_todo_list
+from app.core.suggestion.todo.service import query_all_todo_list, insert_to_my_todo, delete_from_my_todo_list, query_my_todo
 
 todo_router = APIRouter(
     prefix='/suggest/todo'
@@ -16,7 +16,7 @@ def get_todo_list(token: str = Depends(oauth2_scheme)):
 
 @todo_router.get('/my-list')
 def get_my_todo_list(token: str = Depends(oauth2_scheme)):
-    pass
+    return query_my_todo(token)
 
 
 @todo_router.post('/{todo_id}')
--- app/core/suggestion/todo/service.py
+++ app/core/suggestion/todo/service.py
@@ -1,6 +1,6 @@
 from app.utils.security.token import get_user_id
 
-from app.utils.dao.mysql.cqrs.subject.suggest.todo.query import query_todo_list
+from app.utils.dao.mysql.cqrs.subject.suggest.todo.query import query_todo_list, query_my_todo_list
 from app.utils.dao.mysql.cqrs.subject.suggest.todo.command import add_my_todo, delete_todo_from_my_list
 
 
@@ -25,3 +25,11 @@ def delete_from_my_todo_list(token: str, todo_id: str):
     user_id = get_user_id(token)
 
     delete_todo_from_my_list(user_id, todo_id)
+
+
+def query_my_todo(token: str):
+    user_id = get_user_id(token)
+
+    return {
+        'my_todo_list': query_my_todo_list(user_id)
+    }
