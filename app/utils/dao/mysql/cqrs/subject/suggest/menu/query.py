27c488fb2089257020e83c7422955335a5353a99
osangu <osangu012@gmail.com>
2022-11-16 14:33:49 +0900

    ADD: query random menu

---
+++ app/utils/dao/mysql/cqrs/subject/suggest/menu/query.py
@@ -0,0 +1,12 @@
+from sqlalchemy.sql import text
+
+from app.utils.dao.mysql.cqrs import dao
+
+
+def get_rand_menu():
+    with dao.execute_query() as engine:
+        return engine(
+            text(
+                'select image_path, brand_name, name  from tbl_menu order by rand() limit 1'
+            )
+        ).one()
