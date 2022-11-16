8d35e45adaef4a358f2e2bbcba6ddd288a0b1a55
osangu <osangu012@gmail.com>
2022-11-16 16:33:18 +0900

    UPDATE: query_subject_element_by_id_and_kind

--- app/utils/dao/mysql/cqrs/subject/query.py
+++ app/utils/dao/mysql/cqrs/subject/query.py
@@ -1,15 +1,16 @@
-from app.utils.dao.mysql.cqrs import dao
+from uuid import UUID
 
 from sqlalchemy import text
 
+from app.utils.dao.mysql.cqrs import dao
 
-def query_subject_title_list_by_kind(kind: str):
-    query = f"select ts.name as subject, ts.title as title, (" \
-            f"select count(tb.subject_name) as bookmark_amount from tbl_bookmark tb where subject_name like ts.name) as bookmark_amount " \
-            f"from tbl_subject ts" \
-            f" where ts.kind like  '{kind}'" \
+
+def query_subject_element_by_user_id_and_kind(user_id: UUID.hex, kind: str):
+    query = f"select ts.name as name, ts.title as title," \
+            f"(select count(tb.subject_name) as _count from tbl_bookmark tb where tb.subject_name like ts.name) as amount," \
+            f"unhex('{user_id}') in (select user_id from tbl_bookmark tb2 where tb2.subject_name like ts.name) as is_marked " \
+            f"from tbl_subject ts where ts.kind like '{kind}' " \
             f"group by ts.name, ts.title;"
 
     with dao.execute_query() as engine:
         return engine(text(query)).all()
-
