30bc9461d9e14a33f99309f5f058ace92c1a8752
osangu <osangu012@gmail.com>
2022-11-17 08:51:32 +0900

    UPDATE: WebToon 정적 문자열

--- app/core/suggestion/webtoon/service.py
+++ app/core/suggestion/webtoon/service.py
@@ -1,5 +1,7 @@
 from uuid import uuid4, UUID
 
+from app.core import Suggestion
+
 from app.utils.security.token import get_user_id
 
 from app.utils.dataset.crawler.operation.webtoon import webtoon_crawler
@@ -7,10 +9,11 @@ from app.utils.dataset.crawler.operation.webtoon import webtoon_crawler
 from app.utils.dao.mysql.cqrs.subject.comand import create_subject
 from app.utils.dao.mysql.cqrs.bookmark.command import create_bookmark, delete_bookmark_by_user_id_and_name
 
+WebToon = Suggestion.WebToon
+
 
 def query_webtoon():
-
-    create_subject(uuid4().bytes, name='WEBTOON', title='오늘의 웹툰', kind='SUGGEST')
+    create_subject(uuid4().bytes, name=WebToon.NAME, title=WebToon.TITLE, kind=Suggestion.KIND)
 
     return webtoon_crawler.crawl()
 
@@ -18,10 +21,10 @@ def query_webtoon():
 def add_webtoon_to_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    create_bookmark(user_id=user_id, name='WEBTOON', kind='SUGGEST', title='오늘의 웹툰')
+    create_bookmark(user_id=user_id, name=WebToon.NAME, title=WebToon.TITLE, kind=Suggestion.KIND)
 
 
 def delete_my_webtoon_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    delete_bookmark_by_user_id_and_name(user_id, 'WEBTOON')
+    delete_bookmark_by_user_id_and_name(user_id, WebToon.NAME)
