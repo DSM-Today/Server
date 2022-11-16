5ea009e0fd74a006429930e135232adc69ab0a7b
osangu <osangu012@gmail.com>
2022-11-17 08:25:27 +0900

    ADD: Suggest 정적 문자열 클래스

---
+++ app/core/__init__.py
@@ -0,0 +1,31 @@
+class Suggestion:
+    KIND = 'SUGGEST'
+
+    class Book:
+        NAME = 'BOOK'
+        TITLE = '오늘의 책'
+
+    class Food:
+        NAME = 'FOOD'
+        TITLE = '오늘의 음식'
+
+    class Menu:
+        NAME = 'MENU'
+        TITLE = '오늘의 메뉴'
+
+    class Movie:
+        NAME = 'MOVIE'
+        TITLE = '오늘의 영화'
+
+    class Music:
+        NAME = 'MUSIC'
+        TITLE = '오늘의 음악'
+
+    class Todo:
+        NAME = 'TODO'
+        TITLE = '오늘의 할 일'
+
+    class WebToon:
+        NAME = 'WEBTOON'
+        TITLE = '오늘의 웹툰'
+
