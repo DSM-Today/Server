8ca3ba3738953f13e0c2ba95b0d125b4e000c077
osangu <osangu012@gmail.com>
2022-11-16 18:36:22 +0900

    ERROR: get current user id

--- app/core/random/service.py
+++ app/core/random/service.py
@@ -1,8 +1,10 @@
+from app.utils.security.token import get_user_id
+
 from app.utils.dao.mysql.cqrs.subject.query import query_subject_element_by_user_id_and_kind
 
 
 def query_random_subject_list(token):
+    user_id = get_user_id(token)
     return {
-        'random_subject_list': query_subject_element_by_user_id_and_kind(token, 'RANDOM')
+        'random_subject_list': query_subject_element_by_user_id_and_kind(user_id, 'RANDOM')
     }
-
