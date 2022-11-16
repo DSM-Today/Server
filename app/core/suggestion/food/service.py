50a3ac527ce5c67f484623198fbd23152598fc08
osangu <osangu012@gmail.com>
2022-11-17 08:44:27 +0900

    UPDATE: Food 정적 문자열

--- app/core/suggestion/food/service.py
+++ app/core/suggestion/food/service.py
@@ -1,5 +1,7 @@
 from uuid import uuid4, UUID
 
+from app.core import Suggestion
+
 from app.utils.security.token import get_user_id
 
 from app.utils.dataset.crawler.operation.food import food_crawler
@@ -7,9 +9,11 @@ from app.utils.dataset.crawler.operation.food import food_crawler
 from app.utils.dao.mysql.cqrs.subject.comand import create_subject
 from app.utils.dao.mysql.cqrs.bookmark.command import create_bookmark, delete_bookmark_by_user_id_and_name
 
+Food = Suggestion.Food
+
 
 def query_food():
-    create_subject(uuid4().bytes, name='FOOD', title='오늘의 음식', kind='SUGGEST')
+    create_subject(uuid4().bytes, name=Food.NAME, title=Food.TITLE, kind=Suggestion.KIND)
 
     return food_crawler.crawl()
 
@@ -17,10 +21,10 @@ def query_food():
 def add_food_to_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    create_bookmark(user_id=user_id, name='FOOD', title='오늘의 음식', kind='SUGGEST')
+    create_bookmark(user_id=user_id, name=Food.NAME, title=Food.TITLE, kind=Suggestion.KIND)
 
 
 def delete_my_food_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    delete_bookmark_by_user_id_and_name(user_id, 'FOOD')
+    delete_bookmark_by_user_id_and_name(user_id, Food.NAME)
