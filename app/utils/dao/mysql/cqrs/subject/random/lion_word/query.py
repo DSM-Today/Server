7fdd743aa9b7a954abdaddc0e1281b63b5b29e0b
osangu <osangu012@gmail.com>
2022-11-16 22:13:44 +0900

    ADD: query random lion word

---
+++ app/utils/dao/mysql/cqrs/subject/random/lion_word/query.py
@@ -0,0 +1,12 @@
+from sqlalchemy.sql import func
+
+from app.utils.dao.mysql.cqrs import dao
+
+from app.utils.dao.mysql.model.subject.random.lion_word import LionWord
+
+
+def query_random_lion_word():
+    with dao.session_scope() as session:
+        return session.query(
+            LionWord.korean, LionWord.chinese, LionWord.content.label('describe')
+        ).order_by(func.rand()).limit(1).one()
