cea31ba6891c2cdd04bfaae74a5f26234b9cde81
osangu <osangu012@gmail.com>
2022-11-16 22:37:30 +0900

    ADD: query random flower

---
+++ app/utils/dao/mysql/cqrs/subject/random/flower/query.py
@@ -0,0 +1,11 @@
+from sqlalchemy.sql import func
+
+from app.utils.dao.mysql.cqrs import dao
+
+from app.utils.dao.mysql.model.subject.random.flower import Flower
+
+
+def query_random_flower():
+    with dao.session_scope() as session:
+        return session.query(Flower.name, Flower.image_path, Flower.fairy_tale)\
+            .order_by(func.rand()).limit(1).one()
