f589596852fadf966f57fa883ee1317df62aae43
osangu <osangu012@gmail.com>
2022-11-17 08:46:49 +0900

    UPDATE: MOVIE 정적 문자열

--- app/core/suggestion/movie/service.py
+++ app/core/suggestion/movie/service.py
@@ -1,14 +1,18 @@
 from uuid import uuid4, UUID
 
+from app.core import Suggestion
+
 from app.utils.security.token import get_user_id
 
 from app.utils.dataset.crawler.operation.movie import movie_crawler
 from app.utils.dao.mysql.cqrs.subject.comand import create_subject
 from app.utils.dao.mysql.cqrs.bookmark.command import create_bookmark, delete_bookmark_by_user_id_and_name
 
+Movie = Suggestion.Movie
+
 
 def query_movie():
-    create_subject(uuid4().bytes, name='MOVIE', title='오늘의 영화', kind='SUGGEST')
+    create_subject(uuid4().bytes, name=Movie.NAME, title=Movie.TITLE, kind=Suggestion.KIND)
 
     return movie_crawler.crawl()
 
@@ -16,10 +20,10 @@ def query_movie():
 def add_movie_to_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    create_bookmark(user_id=user_id, name='MOVIE', title='오늘의 영화', kind='SUGGEST')
+    create_bookmark(user_id=user_id, name=Movie.NAME, title=Movie.TITLE, kind=Suggestion.KIND)
 
 
 def delete_my_movie_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    delete_bookmark_by_user_id_and_name(user_id, 'MOVIE')
+    delete_bookmark_by_user_id_and_name(user_id, Movie.NAME)
