9ed021bfce98ccc05be81c9b4ae1c4e79e7871c3
osangu <osangu012@gmail.com>
2022-11-16 23:58:08 +0900

    UPDATE: refresh 조건 추가

--- app/core/auth/service.py
+++ app/core/auth/service.py
@@ -7,21 +7,18 @@ from app.config import JWTConfig
 from app.utils.dao.redis.command import set_ex
 from app.utils.dao.redis.query import get_value_by_user_id
 
+from app.utils.dao.mysql.cqrs.user.query import query_user_by_id
+
 from app.utils.security.token import is_refresh_token, get_user_id, generate_access_token, generate_refresh_token
 
 
 def reissue_both_token(token: str):
-    if not is_refresh_token(token):
-        raise HTTPException(
-            status_code=401,
-            detail="INVALID TOKEN"
-        )
-
     user_id = get_user_id(token)
 
+    user = query_user_by_id(user_id)
     refresh_token = get_value_by_user_id(user_id)
 
-    if refresh_token is None:
+    if (refresh_token is None) or (user is None) or not is_refresh_token(token):
         raise HTTPException(
             status_code=401,
             detail="INVALID TOKEN"
