aac50d78f2337255ae2e73d46b2dc3b399d3775e
osangu <osangu012@gmail.com>
2022-11-18 03:48:39 +0900

    REFACTOR: lion word 패키지 변경

--- app/core/subject/random/lion/service.py
+++ app/core/subject/random/lion/service.py
@@ -1,4 +1,4 @@
-from app.utils.dao.mysql.cqrs.subject.random.lion_word.query import query_random_lion_word
+from app.utils.dao.mysql.subject.random.lion_word.query import query_random_lion_word
 
 
 def query_lion_word():
