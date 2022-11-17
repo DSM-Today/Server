0b888a8fd8e865d484beaf5b0780809ae907b3ed
osangu <osangu012@gmail.com>
2022-11-18 04:39:13 +0900

    ADD: str_to_month_with_day

--- app/utils/type_changer.py
+++ app/utils/type_changer.py
@@ -5,5 +5,10 @@ def str_to_date(origin_str: str):
     return datetime.strptime(origin_str, '%Y%m%d').date()
 
 
+def str_to_month_with_day(origin_str: str):
+    origin_date = datetime.strptime(origin_str, '%m%d').date()
+    return f'{origin_date.month}-{origin_date.day}'
+
+
 def date_to_int(from_data: date):
     return int(from_data.strftime('%Y%m%d').replace('-', ''))
