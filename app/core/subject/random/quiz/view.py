fccf4f348584d736df3c50fd33bf868dae45c7ca
osangu <osangu012@gmail.com>
2022-11-18 02:06:22 +0900

    UDPATE: status code & exception hande

--- app/core/subject/random/quiz/view.py
+++ app/core/subject/random/quiz/view.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+
+from app.utils import show_reason
 
 from app.core.subject.random.quiz.service import get_quiz
 
@@ -7,6 +9,7 @@ quiz_router = APIRouter(
 )
 
 
-@quiz_router.get('/')
+@show_reason
+@quiz_router.get('/', status_code=status.HTTP_200_OK)
 def get_random_quiz():
     return get_quiz()
