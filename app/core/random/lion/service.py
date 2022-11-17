4e1f97240ed1554dc7f62656ff445026b6e5c56e
osangu <osangu012@gmail.com>
2022-11-17 09:13:34 +0900

    UPDATE: LION WORD 정적 문자열

--- app/core/random/lion/service.py
+++ app/core/random/lion/service.py
@@ -1,10 +1,14 @@
 from uuid import UUID
 
+from app.core import Random
+
 from app.utils.security.token import get_user_id
 
 from app.utils.dao.mysql.cqrs.bookmark.command import create_bookmark, delete_bookmark_by_user_id_and_name
 
 from app.utils.dao.mysql.cqrs.subject.random.lion_word.query import query_random_lion_word
+
+LionWord = Random.LionWord
 
 
 def query_lion_word():
@@ -14,15 +18,10 @@ def query_lion_word():
 def add_lion_to_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    create_bookmark(
-        user_id,
-        name='LION_WORD',
-        kind='RANDOM',
-        title='오늘의 사자성어'
-    )
+    create_bookmark(user_id, name=LionWord.NAME, title=LionWord.TITLE, kind=Random.KIND)
 
 
 def delete_my_lion_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    delete_bookmark_by_user_id_and_name(user_id, 'LION_WORD')
+    delete_bookmark_by_user_id_and_name(user_id, LionWord.NAME)
