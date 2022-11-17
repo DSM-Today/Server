fa6abba807566134cf71f249e9a37b797bdb8a55
osangu <osangu012@gmail.com>
2022-11-18 02:26:10 +0900

    UPDATE: random list 정적 문자열

--- app/core/subject/random/service.py
+++ app/core/subject/random/service.py
@@ -1,3 +1,5 @@
+from app.core.subject.random import Random
+
 from app.utils.security.token import get_user_id
 
 from app.utils.dao.mysql.cqrs.subject.query import query_subject_element_by_user_id_and_kind
@@ -6,5 +8,5 @@ from app.utils.dao.mysql.cqrs.subject.query import query_subject_element_by_user
 def query_random_subject_list(token):
     user_id = get_user_id(token)
     return {
-        'random_subject_list': query_subject_element_by_user_id_and_kind(user_id, 'RANDOM')
+        'random_subject_list': query_subject_element_by_user_id_and_kind(user_id, Random.KIND)
     }
