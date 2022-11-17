df29e32d6c9aea48d472aff42ff426617268cd39
osangu <osangu012@gmail.com>
2022-11-17 22:50:28 +0900

    ADD: food query

---
+++ app/utils/dao/mysql/cqrs/subject/suggest/food/query.py
@@ -0,0 +1,12 @@
+from sqlalchemy.sql import func
+
+from app.utils.dao.mysql.cqrs import dao
+from app.utils.dao.mysql.model.subject.suggest.food import Food
+
+
+def query_random_food():
+    with dao.session_scope() as session:
+        return session.query(
+            Food.name, Food.image_path
+        )\
+            .order_by(func.rand()).limit(1).one()
