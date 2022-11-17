77bc436efde341f63c6943338f926e56b8d46347
osangu <osangu012@gmail.com>
2022-11-18 03:59:44 +0900

    REFACTOR: query_random_moive 패키지 변경

--- app/core/subject/suggest/movie/service.py
+++ app/core/subject/suggest/movie/service.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 
 from app.core.subject.suggest.movie import Movie
 
-from app.utils.dao.mysql.cqrs.subject.suggest.movie.query import query_random_movie
+from app.utils.dao.mysql.subject.suggest.movie.query import query_random_movie
 from app.utils.dao.mysql.subject.comand import create_subject
 
 
