0c02c1450a06a4b6c480cea0dc1472ebcb0b9b98
osangu <osangu012@gmail.com>
2022-11-17 09:07:50 +0900

    UPDATE: Flower 정적 문자열

--- app/core/random/flower/service.py
+++ app/core/random/flower/service.py
@@ -1,10 +1,14 @@
 from uuid import UUID
 
+from app.core import Random
+
 from app.utils.security.token import get_user_id
 
 from app.utils.dao.mysql.cqrs.subject.random.flower.query import query_random_flower
 
 from app.utils.dao.mysql.cqrs.bookmark.command import create_bookmark, delete_bookmark_by_user_id_and_name
+
+Flower = Random.Flower
 
 
 def query_flower():
@@ -14,15 +18,10 @@ def query_flower():
 def add_flower_to_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    create_bookmark(
-        user_id,
-        name='FLOWER',
-        kind='RANDOM',
-        title='오늘의 꽃'
-    )
+    create_bookmark(user_id, name=Flower.NAME, title=Flower.TITLE, kind=Random.KIND)
 
 
 def delete_my_flower_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    delete_bookmark_by_user_id_and_name(user_id, 'FLOWER')
+    delete_bookmark_by_user_id_and_name(user_id, Flower.NAME)
