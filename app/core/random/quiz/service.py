59d79b7d08b1adce80858723ef462f6860ef4a63
osangu <osangu012@gmail.com>
2022-11-17 09:26:47 +0900

    UPDATE: QUIZ 정적 문자열

--- app/core/random/quiz/service.py
+++ app/core/random/quiz/service.py
@@ -1,10 +1,14 @@
 from uuid import UUID
 
+from app.core import Random
+
 from app.utils.security.token import get_user_id
 
 from app.utils.dao.mysql.cqrs.bookmark.command import create_bookmark, delete_bookmark_by_user_id_and_name
 
 from app.utils.dao.mysql.cqrs.subject.random.quiz.query import get_rand_quiz
+
+Quiz = Random.Quiz
 
 
 def get_quiz():
@@ -14,17 +18,10 @@ def get_quiz():
 def add_quiz_to_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    create_bookmark(
-        user_id,
-        name='QUIZ',
-        kind='RANDOM',
-        title='오늘의 퀴즈'
-    )
+    create_bookmark(user_id, name=Quiz.NAME, title=Quiz.TITLE, kind=Random.KIND)
 
 
 def delete_my_quiz_bookmark(token: str):
-
     user_id = UUID(get_user_id(token)).hex
 
-    delete_bookmark_by_user_id_and_name(user_id, 'QUIZ')
-
+    delete_bookmark_by_user_id_and_name(user_id, subject_name=Quiz.NAME)
