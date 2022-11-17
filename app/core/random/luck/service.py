90ef932882733ac074833425e1f6c0e185023785
osangu <osangu012@gmail.com>
2022-11-17 09:26:41 +0900

    UPDATE: LUCK 정적 문자열

--- app/core/random/luck/service.py
+++ app/core/random/luck/service.py
@@ -1,5 +1,7 @@
 from uuid import uuid4, UUID
 
+from app.core import Random
+
 from app.utils.security.token import get_user_id
 
 from app.utils.dataset.crawler.random.lucky import luck_crawler
@@ -8,32 +10,28 @@ from app.utils.dao.mysql.cqrs.subject.comand import create_subject
 from app.utils.dao.mysql.cqrs.bookmark.command import create_bookmark, delete_bookmark_by_user_id_and_name
 from app.utils.dao.mysql.cqrs.user.query import query_user_birth_by_id
 
+Luck = Random.Luck
+
 
 def query_user_luck(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    create_subject(uuid4().bytes, kind='RANDOM', name='LUCKY', title='오늘의 운세')
+    create_subject(uuid4().bytes, name=Luck.NAME, title=Luck.TITLE, kind=Random.KIND)
 
     birth = query_user_birth_by_id(user_id)
 
     return luck_crawler.crawl(
-        int(str(birth)[5:].replace('-', ''))
+        int(str(birth)[5:].replace('-', ''))  # TODO
     )
 
 
 def add_my_lucky_bookmark(token: str):
     user_id = UUID(get_user_id(token)).hex
 
-    create_bookmark(
-        user_id,
-        name='LUCKY',
-        kind='RANDOM',
-        title='오늘의 운세'
-    )
+    create_bookmark(user_id, name=Luck.NAME, title=Luck.TITLE, kind=Random.KIND)
 
 
 def delete_my_lucky_bookmark(token: str):
-
     user_id = UUID(get_user_id(token)).hex
 
-    delete_bookmark_by_user_id_and_name(user_id, 'LUCKY')
+    delete_bookmark_by_user_id_and_name(user_id, Luck.NAME)
