27b5ae14a795b8e6a675ed28cec93dd8e6bd2eab
osangu <osangu012@gmail.com>
2022-11-17 23:49:19 +0900

    UPDATE: self session

--- app/utils/dao/mysql/cqrs/__init__.py
+++ app/utils/dao/mysql/cqrs/__init__.py
@@ -12,8 +12,7 @@ class DAO:
 
     def __init__(self):
         self.__engine = self.__create_engine()
-
-    #        self.__session = self.__create_session(self.__engine)()
+        self.__session = self.__create_session(self.__engine)()
 
     @staticmethod
     def __create_engine() -> Engine:
@@ -34,14 +33,13 @@ class DAO:
 
     @contextmanager
     def session_scope(self) -> Session:
-        session = self.__create_session(self.__engine)()
         try:
-            yield session
-            session.commit()
+            yield self.__session
+            self.__session.commit()
         except:
-            session.rollback()
+            self.__session.rollback()
         finally:
-            session.close()
+            self.__session.close()
 
     @contextmanager
     def execute_query(self):
