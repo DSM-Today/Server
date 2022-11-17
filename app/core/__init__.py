92730b3b1eb2f25e87397357cac7ff673dc59039
osangu <osangu012@gmail.com>
2022-11-17 09:05:25 +0900

    ADD: Random 정적 문자열 클래스

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -29,3 +29,26 @@ class Suggestion:
         NAME = 'WEBTOON'
         TITLE = '오늘의 웹툰'
 
+
+class Random:
+    KIND = 'RANDOM'
+
+    class Flower:
+        NAME = 'FLOWER'
+        TITLE = '오늘의 꽃'
+
+    class LionWord:
+        NAME = 'LION_WORD'
+        TITLE = '오늘의 사자성어'
+
+    class Luck:
+        NAME = 'LUCKY'
+        TITLE = '오늘의 운세'
+
+    class Person:
+        NAME = 'PERSON'
+        TITLE = '오늘의 인물'
+
+    class Quiz:
+        NAME = 'QUIZ'
+        TITLE = '오늘의 퀴즈'
