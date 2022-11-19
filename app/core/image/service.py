ed21c5bfbe5dc038ab30bb553ad972c84e926507
osangu <osangu012@gmail.com>
2022-11-19 17:46:12 +0900

    UPDATE: random_file_name

--- app/core/image/service.py
+++ app/core/image/service.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 import boto3
 from fastapi import UploadFile
 
@@ -11,13 +13,14 @@ __s3_client = boto3.client(
 
 
 def upload_file_on_s3(file: UploadFile):
+    random_file_name = uuid4().hex
     __s3_client.upload_fileobj(
         file.file,
         S3Config.BUCKET_NAME,
-        file.filename,
+        random_file_name,
         ExtraArgs={'ContentType': 'image/jpeg'}
     )
 
     return {
-        'image_path': f'https://{S3Config.BUCKET_NAME}.s3.{S3Config.LOCATION}.amazonaws.com/{file.filename}'
+        'image_path': f'https://{S3Config.BUCKET_NAME}.s3.{S3Config.LOCATION}.amazonaws.com/{random_file_name}'
     }
