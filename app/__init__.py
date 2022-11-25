820d8dbd91753e11765f5817517ac0e6b40425ac
osangu <osangu012@gmail.com>
2022-11-25 11:59:33 +0900

    (#3)_refactor :: include_random_router 위치 변경

--- app/__init__.py
+++ app/__init__.py
@@ -5,19 +5,12 @@ from app.core.auth.oauth import oauth_router
 
 from app.core.user import include_user_router
 
+from app.core.subject.random import include_random_routers
 from app.core.subject.suggest import include_suggest_router
-
 
 from app.core.subject.information.view import information_router
 from app.core.subject.information.news.view import news_router
 from app.core.subject.information.lotto.view import lotto_router
-
-from app.core.subject.random.view import random_router
-from app.core.subject.random.quiz.view import quiz_router
-from app.core.subject.random.lion.view import lion_router
-from app.core.subject.random.luck.view import luck_router
-from app.core.subject.random.flower.view import flower_router
-from app.core.subject.random.person.view import person_router
 
 from app.core.bookmark.view import bookmark_router
 
@@ -31,17 +24,6 @@ def _include_bookmark_router(app: FastAPI):
 def _include_auth_router(app: FastAPI):
     app.include_router(auth_router)
     app.include_router(oauth_router)
-
-
-
-
-def _include_random_routers(app: FastAPI):
-    app.include_router(random_router)
-    app.include_router(luck_router)
-    app.include_router(quiz_router)
-    app.include_router(flower_router)
-    app.include_router(lion_router)
-    app.include_router(person_router)
 
 
 def _include_information_routers(app: FastAPI):
@@ -58,7 +40,7 @@ def create_app():
     include_image_router(app)
 
     _include_information_routers(app)
-    _include_random_routers(app)
+    include_random_routers(app)
     include_suggest_router(app)
 
     _include_auth_router(app)
--- app/core/subject/random/__init__.py
+++ app/core/subject/random/__init__.py
@@ -1,5 +1,23 @@
+from fastapi import FastAPI
+
 from app.core.subject import Subject
 
 
 class Random(Subject):
     KIND = 'RANDOM'
+
+
+def include_random_routers(app: FastAPI):
+    from app.core.subject.random.view import random_router
+    from app.core.subject.random.quiz.view import quiz_router
+    from app.core.subject.random.lion.view import lion_router
+    from app.core.subject.random.luck.view import luck_router
+    from app.core.subject.random.flower.view import flower_router
+    from app.core.subject.random.person.view import person_router
+
+    app.include_router(random_router)
+    app.include_router(quiz_router)
+    app.include_router(lion_router)
+    app.include_router(luck_router)
+    app.include_router(flower_router)
+    app.include_router(person_router)
