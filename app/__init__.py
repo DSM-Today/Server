038bc6aa5db6b252d4823c768a084e1780f14ac1
osangu <osangu012@gmail.com>
2022-11-25 12:04:50 +0900

    (#3)_refactor :: include_bookmark_router 위치 변경

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,13 @@ from app.core.auth.oauth import oauth_router
 
 from app.core.user import include_user_router
 
+from app.core.image import include_image_router
+
+from app.core.bookmark import include_bookmark_router
+
 from app.core.subject.random import include_random_routers
 from app.core.subject.suggest import include_suggest_routers
 from app.core.subject.information import include_information_routers
-
-from app.core.bookmark.view import bookmark_router
-
-from app.core.image import include_image_router
-
-
-def _include_bookmark_router(app: FastAPI):
-    app.include_router(bookmark_router)
 
 
 def _include_auth_router(app: FastAPI):
@@ -30,11 +26,12 @@ def create_app():
 
     include_image_router(app)
 
+    include_bookmark_router(app)
+
     include_random_routers(app)
     include_suggest_routers(app)
     include_information_routers(app)
 
     _include_auth_router(app)
-    _include_bookmark_router(app)
 
     return app
