ee42fc7b13047cf2df9765d4633d558233e643bf
osangu <osangu012@gmail.com>
2022-11-27 12:50:58 +0900

    (#4)_refactor :: 이중 클래스명 변경

--- app/core/auth/oauth/view.py
+++ app/core/auth/oauth/view.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Response, status
 
-from app.core.auth.oauth.dto import DTO
+from app.core.auth.oauth.dto import Request
 
 from app.core.auth.oauth.service import query_client_id, register_or_login
 
@@ -15,5 +15,5 @@ def get_oauth_link(service_type: str):
 
 
 @oauth_router.post('/{service_type}', status_code=status.HTTP_201_CREATED)
-def get_code(service_type, request: DTO.IdTokenRequest, response: Response):
+def get_code(service_type, request: Request.IdToken, response: Response):
     return register_or_login(service_type, request.id_token, response)
